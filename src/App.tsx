26b341f3bb18f511bfe9e3e1cd5981eb5013f00e
Дина Гудкова <dinagudkova@MacBook-Air-Dina.local>
2025-10-30 14:04:39 +0100

    Initial commit: React + TypeScript + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-8 text-center">
+      <h1 className="text-4xl font-bold">Local Weather App</h1>
+    </div>
+  )
+}
+
+export default App
